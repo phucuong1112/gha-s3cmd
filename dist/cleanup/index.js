2834ce8c23b8bd6369760f875714ef3ffd5b05bd
cuongnp <phucuong1112@gmail.com>
2024-03-22 16:46:47 +0700

    fix: console log

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -26601,7 +26601,7 @@ core.info(`* Clean up file ${cleanup_config}`);
 if (['1', "true", "yes"].indexOf(cleanup_config.toLowerCase()) >= 0) {
   let config_path = core.getInput("config_path") || path.join(homedir, '.s3cfg');
   fs.rmSync(config_path);
-  core.info(`- ${cleanup_config} is deleted`);
+  core.info(`- ${config_path} is deleted`);
 }
 
 return 0
